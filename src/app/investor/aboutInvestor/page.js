23998b4fbb77df36f1658f4e11844d6875e1004e
Akash Mohan Rao Deshmukh <akash.mohan.rao.deshmukh@ibm.com>
2025-07-09 17:22:12 +0530

    new changes

--- src/app/investor/aboutInvestor/page.js
+++ src/app/investor/aboutInvestor/page.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 export const metadata = {
-  title: "About investor",
+  title: "About Page",
   description: "All details about page",
   authors: [{name:"vinod thapa"}, {name: "thapa technical"}, {url:"myvi.com"}],
   keywords: ["vi","myvi"]
 }
-export const aboutInvestor = () => {
+
+export default function about() {
   return (
     <div>about investor</div>
   )
