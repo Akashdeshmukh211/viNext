6895ba56e51718d8adbe4292ed65b20f07c81679
Akash Mohan Rao Deshmukh <akash.mohan.rao.deshmukh@ibm.com>
2025-07-09 17:08:58 +0530

    new changes

---
+++ src/app/investor/aboutInvestor/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+  title: "About investor",
+  description: "All details about page",
+  authors: [{name:"vinod thapa"}, {name: "thapa technical"}, {url:"myvi.com"}],
+  keywords: ["vi","myvi"]
+}
+export const aboutInvestor = () => {
+  return (
+    <div>about investor</div>
+  )
+}
